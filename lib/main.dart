c00da6e609b80c6f07a8c014a1c13cc942c861ac
Rijk-linux <20376316@sun.ac.za>
2020-06-24 21:31:25 +0200

    Added details, added avatar border, added extra fonts

--- lib/main.dart
+++ lib/main.dart
@@ -27,12 +27,82 @@ class MyApp extends StatelessWidget {
                   width: double.infinity,
                 ),
                 CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage('images/me.jpg'),
+                  radius: 53,
+                  backgroundColor: Colors.white,
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('images/me.jpg'),
+                  ),
                 ),
                 Text(
                   'Rijk de Wet',
-                  style: TextStyle(fontSize: 35, color: Colors.white, fontFamily: 'Yeseva One'),
+                  style: TextStyle(
+                    fontSize: 45,
+                    color: Colors.white,
+                    fontFamily: 'Lobster',
+                  ),
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.teal[100],
+                    fontFamily: 'Source Sans Pro',
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        '076 139 8409',
+                        style: TextStyle(
+                          color: Colors.teal[800],
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 25),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'rijkdw1@gmail.com',
+                        style: TextStyle(
+                          color: Colors.teal[800],
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
